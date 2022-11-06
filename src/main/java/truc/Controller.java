8ba4aaeeef8908f0427225415bea9e365f3e13e5
Constance <79022298+knstnce@users.noreply.github.com>
2022-11-06 07:25:45 -0500

    Modified the Controller, the button now works too

--- src/main/java/truc/Controller.java
+++ src/main/java/truc/Controller.java
@@ -1,29 +1,59 @@
 package truc;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 
+
 public class Controller {
 
     // All the fxml elements
-    @FXML public Pane pane;
-    @FXML public Label label;
-    @FXML public TextField username;
-    @FXML public PasswordField password;
-    @FXML public TextField textfield;
-    @FXML public CheckBox checkbox;
-    @FXML public Button button;
+    @FXML
+    public Pane pane;
+    @FXML
+    public Label label;
+    @FXML
+    public TextField username;
+    @FXML
+    public PasswordField password;
+    @FXML
+    public TextField textfield;
+    @FXML
+    public CheckBox checkbox;
+    @FXML
+    public Button button;
+
+    private int counter = 0;
 
     // Dummy controller for the FXML file
     public Controller() {
     }
 
-    // The FXML file will call this method (after the controller), it will set the right password fields
-    @FXML public void initialize() {
-        checkbox.setOnAction(event -> {
+    // The FXML file will call this method when being created (after the constructor),
+    // it will set the right password fields and set the button action
+    @FXML
+    public void initialize() {
+        changeVisibility();
+
+        button.setOnAction(buttonEvent -> {
+            if (counter == 0) {
+                counter ++;
+                System.out.println("no");
+            } else {
+                System.out.println("yes");
+            }
+        });
+    }
+
+    // When the checkbox is clicked, the event will be triggered
+    @FXML
+    public void handleCheckbox() {
+        changeVisibility();
+    }
+
+    // Changes the visibility of the password field
+    private void changeVisibility() {
+        checkbox.setOnAction(checkboxEvent -> {
             if (checkbox.isSelected()) {
                 password.setVisible(false);
                 textfield.setVisible(true);
@@ -34,44 +64,5 @@ public class Controller {
                 password.setText(textfield.getText());
             }
         });
-    }
-
-    // When the checkbox is clicked, the event will be triggered
-    @FXML public void handleCheckbox() {
-        checkbox.setOnAction(eh);
-    }
-
-    // Event Handler description
-    EventHandler<ActionEvent> eh = new EventHandler<>() {
-        @Override
-        public void handle(ActionEvent event) {
-            if (event.getSource() instanceof CheckBox) {
-                if (checkbox.isSelected()) {
-                    buildTheBox(textfield);
-                } else {
-                    buildTheBox(password);
-                }
-            }
-        }
-    };
-
-    // Clears the pane and adds the right password field
-    private void buildTheBox(TextField textfield) {
-        pane.getChildren().clear();
-        pane.getChildren().add(label);
-        username.setLayoutX(118);
-        username.setLayoutY(42);
-        pane.getChildren().add(username);
-        username.setLayoutX(170);
-        username.setLayoutY(103);
-        pane.getChildren().add(textfield);
-        textfield.setLayoutX(170);
-        textfield.setLayoutY(148);
-        pane.getChildren().add(checkbox);
-        checkbox.setLayoutX(195);
-        checkbox.setLayoutY(189);
-        pane.getChildren().add(button);
-        button.setLayoutX(223);
-        button.setLayoutY(221);
     }
 }
