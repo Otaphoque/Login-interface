9312d7d94ea0f117edadf9f091ad5a353786e758
Constance <79022298+knstnce@users.noreply.github.com>
2022-11-07 17:13:15 -0500

    Project completed ! (Added a label saying incorrect password compared to the previous version

--- src/main/java/truc/Controller.java
+++ src/main/java/truc/Controller.java
@@ -1,9 +1,12 @@
 package truc;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Transition;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.util.Duration;
 
 
 public class Controller {
@@ -14,7 +17,7 @@ public class Controller {
     @FXML
     public Pane pane;
     @FXML
-    public Label label;
+    public Label mainLabel;
     @FXML
     public TextField username;
     @FXML
@@ -25,6 +28,8 @@ public class Controller {
     public CheckBox checkbox;
     @FXML
     public Button button;
+    @FXML
+    public Label label;
 
     public final String login = "constance_prevot";
     public final String code = "Fall_2023!";
@@ -38,12 +43,15 @@ public class Controller {
     @FXML
     public void initialize() {
         this.textfield.setVisible(false);
+        this.label.setOpacity(0);
+        this.checkbox.setText(" Show password");
         changeVisibility();
 
         button.setOnAction(buttonEvent -> {
             if (username.getText().equals(login) && (password.getText().equals(code) || textfield.getText().equals(code))) {
                 System.out.println("yes");
             } else {
+                this.handleAnimation();
                 System.out.println("no");
             }
         });
@@ -55,14 +63,27 @@ public class Controller {
         changeVisibility();
     }
 
+    private void handleAnimation() {
+        this.label.setOpacity(1);
+        FadeTransition transition = new FadeTransition();
+        transition.setDuration(Duration.millis(500));
+        transition.setFromValue(1);
+        transition.setToValue(0);
+        transition.setDelay(Duration.millis(1000));
+        transition.setNode(label);
+        transition.play();
+    }
+
     // Changes the visibility of the password field
     private void changeVisibility() {
         checkbox.setOnAction(checkboxEvent -> {
             if (checkbox.isSelected()) {
+                checkbox.setText(" Hide password");
                 password.setVisible(false);
                 textfield.setVisible(true);
                 textfield.setText(password.getText());
             } else {
+                checkbox.setText(" Show password");
                 password.setVisible(true);
                 textfield.setVisible(false);
                 password.setText(textfield.getText());
